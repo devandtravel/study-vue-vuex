a4265bf50be5959d9841aa3f8c9081c29ff29f6a
devandtravel <devandtravel@gmail.com>
2021-08-25 15:31:16 +0300

    add: render data from jsonPlaceholder without Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="post" v-for="post in posts" :key="post.id">
+      <h2>{{post.title}}</h2>
+      <p>{{post.body}}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      posts:[]
+    }
+  },
+  async mounted(){
+    const responce = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+    const posts = await responce.json()
+    this.posts = posts
   }
 }
 </script>
@@ -21,6 +30,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  width: 400px;
+}
+.post{
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-bottom: 1rem;
 }
 </style>
